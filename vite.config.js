de97b475ac79d052ee487bcac3bf99457fd46c3d
Shashikanth Bhat <shashikanthmbhat@gmail.com>
2026-08-30 22:38:30 +0530

    Modify base path based on environment variable
    Update base path for GitHub Actions environment.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/New-Portpolio-Website/",
-})
+  base: process.env.GITHUB_ACTIONS
+    ? "/New-Portpolio-Website/"
+    : "/",
+});
